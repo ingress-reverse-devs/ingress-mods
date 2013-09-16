fbce80914bdc283f7ca49eed069b80fd63b5d210
radopi <me@me.com>
2013-09-16 13:14:24 +0200

    Added patch to prevent Comm/OPS from burnung in fast

--- patch.sh
+++ patch.sh
@@ -1,5 +1,3 @@
-$apkfile=$1
-$outputfile=$2
 cmd=(dialog --separate-output --checklist "Select options:" 22 76 16)
 options=(1 "Rosa Resistance" off    # any option can be set to default to "on"
          2 "Landscape" off
@@ -13,5 +11,11 @@ if [ ! -e $(basename $url) ]
     then wget $url
 fi
 java -jar apktool.jar d -f $(basename "$url") out-dir
-#rm ingress-1.35.1-rosac.apk; java -jar apktool.jar b ingress-1.35.1-broot-1.0.2.11-mute ingress-1.35.1-rosa.apk &&  zipalign -v 4 ingress-1.35.1-rosa.apk ingress-1.35.1-rosac.apk && jarsigner -verbose -keystore debug.keystore  -keypass "#include <stdio.h>" ingress-1.35.1-rosac.apk tomle_singing && adb install -r ingress-1.35.1-rosac.apk
+for choice in $choices
+do
+    case $choice in
+        1)
+            patch -p0 patch-rosa
+done
+rm ingress-1.35.1-out.apk; java -jar apktool.jar b out-dir ingress-1.35.1-out.apk &&  zipalign -v 4 ingress-1.35.1-out.apk ingress-1.35.1-out-signed.apk && jarsigner -verbose -keystore debug.keystore ingress-1.35.1-out-signed.apk ingress-reverse-devs && adb install -r ingress-1.35.1-out-signes.apk && rm ingress-1.35.1-out.apk
 
