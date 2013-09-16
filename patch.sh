bc9674d42654d0565ca78386740942650ddea94b
radopi <me@me.com>
2013-09-16 13:19:56 +0200

    Modified patsh-dark and patsh.sh to patsh the right way

--- patch.sh
+++ patch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cmd=(dialog --separate-output --checklist "Select options:" 22 76 16)
 options=(1 "Rosa Resistance" off    # any option can be set to default to "on"
          2 "Landscape" off
@@ -11,11 +12,21 @@ if [ ! -e $(basename $url) ]
     then wget $url
 fi
 java -jar apktool.jar d -f $(basename "$url") out-dir
+echo "going to patch"
 for choice in $choices
 do
+    echo "patching"
     case $choice in
         1)
-            patch -p0 patch-rosa
+            echo "patch1"
+            patch -p0 -i patch-rosa
+            ;;
+        3)
+            echo "patch2"
+            patch -p0 -i patch-dark
+            for i in dark/*.png; do cp $i out-dir/assets/packed/$(basename $i .png)/common.png; done
+            ;;
+    esac
 done
-rm ingress-1.35.1-out.apk; java -jar apktool.jar b out-dir ingress-1.35.1-out.apk &&  zipalign -v 4 ingress-1.35.1-out.apk ingress-1.35.1-out-signed.apk && jarsigner -verbose -keystore debug.keystore ingress-1.35.1-out-signed.apk ingress-reverse-devs && adb install -r ingress-1.35.1-out-signes.apk && rm ingress-1.35.1-out.apk
+rm ingress-1.35.1-out-signed.apk; java -jar apktool.jar b out-dir ingress-1.35.1-out.apk &&  zipalign -v 4 ingress-1.35.1-out.apk ingress-1.35.1-out-signed.apk && jarsigner -verbose -keystore debug.keystore ingress-1.35.1-out-signed.apk ingress-reverse-devs && adb install -r ingress-1.35.1-out-signed.apk && rm ingress-1.35.1-out.apk
 
